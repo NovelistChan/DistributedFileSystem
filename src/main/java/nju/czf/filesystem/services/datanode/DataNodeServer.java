b1424ec1648bf6a1c861bb4cfea0a7eb2c4bfa19
Novel <novelist.chan@gmail.com>
2019-06-03 21:19:45 +0800

    framework display

---
+++ src/main/java/nju/czf/filesystem/services/datanode/DataNodeServer.java
@@ -0,0 +1,10 @@
+package nju.czf.filesystem.services.datanode;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DataNodeServer {
+}
---
+++ src/main/java/nju/czf/filesystem/services/namenode/NameNodeServer.java
@@ -0,0 +1,9 @@
+package nju.czf.filesystem.services.namenode;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NameNodeServer {
+}
